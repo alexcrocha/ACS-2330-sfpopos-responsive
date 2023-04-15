95d646bb6e19157c6501fa3cd74dd87c481bee46
Alex Rocha <rocha.amc@gmail.com>
2023-04-15 16:53:50 -0700

    Update About view

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import data from '../../sfpopos-data';
 import './About.css';
 
 function About() {
+  const randomSite = data[Math.floor(Math.random() * data.length)];
+  const image = randomSite.images[0];
+  const { title } = randomSite;
   return (
     <div className="About">
-      <h1>About SFPOPOS</h1>
-      <p>
-        POPOS are publicly accessible spaces in
-        forms of plazas, terraces, atriums, small
-        parks, and even snippets which are provided
-        and maintained by private developers. In San
-        Francisco, POPOS mostly appear in the Downtown
-        office district area.
-      </p>
+      <div className="About-image">
+        <img src={`${process.env.PUBLIC_URL}images/${image}`} alt={title} />
+      </div>
+      <div className="About-info">
+        <h1>About SFPOPOS</h1>
+        <p>
+          POPOS are publicly accessible spaces in
+          forms of plazas, terraces, atriums, small
+          parks, and even snippets which are provided
+          and maintained by private developers. In San
+          Francisco, POPOS mostly appear in the Downtown
+          office district area.
+        </p>
+      </div>
     </div>
   );
 }
